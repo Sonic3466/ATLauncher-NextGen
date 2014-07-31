5653d167830fc92fc49608426f03f522f5331413
Asyncronous <asyncronous16@gmail.com>
2014-07-30 21:41:33 -0400

    test

---
+++ Core/src/main/java/com/atlauncher/ui/comp/TwoStateButton.java
@@ -0,0 +1,12 @@
+package com.atlauncher.ui.comp;
+
+import javax.swing.JButton;
+import javax.swing.plaf.basic.BasicButtonUI;
+
+public final class TwoStateButton
+extends JButton{
+    private final class TwoStateButtonUI
+    extends BasicButtonUI{
+        
+    }
+}
