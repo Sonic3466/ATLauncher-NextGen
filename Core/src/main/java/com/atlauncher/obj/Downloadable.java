8daf08ccd314535bd870df91475ab9a5485c33ed
RyanTheAllmighty <rldowling03@gmail.com>
2014-11-11 15:47:11 +1100

    Check for server availability when starting up

--- Core/src/main/java/com/atlauncher/obj/Downloadable.java
+++ Core/src/main/java/com/atlauncher/obj/Downloadable.java
@@ -34,8 +34,7 @@ import java.nio.file.StandardOpenOption;
 import java.util.EnumSet;
 import java.util.zip.GZIPInputStream;
 
-public final class Downloadable
-implements Runnable{
+public final class Downloadable implements Runnable {
     public final String endpoint;
     public final String hash;
     public final Path output;
@@ -43,29 +42,39 @@ implements Runnable{
 
     private HttpURLConnection conn;
 
-    public Downloadable(String endpoint, Path output, String hash, boolean md5){
+    public Downloadable(String endpoint, Path output) {
+        this(endpoint, output, null, true);
+    }
+
+    public Downloadable(String endpoint, Path output, String hash, boolean md5) {
         this.endpoint = endpoint;
         this.hash = hash;
         this.md5 = md5;
         this.output = output.resolve(endpoint.substring(endpoint.lastIndexOf('/') + 1));
 
-        try{
-            if(!Files.exists(output.getParent())){
+        try {
+            if (!Files.exists(output.getParent())) {
                 Files.createDirectories(output.getParent());
             }
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace(System.out);
         }
     }
 
-    private boolean isGzip(){
-        return (this.getConnection().getContentEncoding() != null && this.getConnection().getContentEncoding().equalsIgnoreCase("gzip"));
+    private boolean isGzip() {
+        return (this.getConnection().getContentEncoding() != null && this.getConnection().getContentEncoding()
+                .equalsIgnoreCase("gzip"));
     }
 
-    private HttpURLConnection getConnection(){
-        if(this.conn == null){
-            try{
-                this.conn = (HttpURLConnection) new URL(Settings.server.getFileURL(this.endpoint)).openConnection();
+    private HttpURLConnection getConnection() {
+        if (this.conn == null) {
+            try {
+                if (this.endpoint.startsWith("http://") || this.endpoint.startsWith("https://")) {
+                    // We provided a full URL so don't try to resolve it
+                    this.conn = (HttpURLConnection) new URL(this.endpoint).openConnection();
+                } else {
+                    this.conn = (HttpURLConnection) new URL(Settings.server.getFileURL(this.endpoint)).openConnection();
+                }
 
                 this.conn.setUseCaches(false);
                 this.conn.setDefaultUseCaches(false);
@@ -75,7 +84,7 @@ implements Runnable{
                 this.conn.setRequestProperty("Expires", "0");
                 this.conn.setRequestProperty("Pragma", "no-cache");
                 this.conn.connect();
-            } catch(Exception ex){
+            } catch (Exception ex) {
                 ex.printStackTrace(System.err);
             }
         }
@@ -83,15 +92,15 @@ implements Runnable{
         return this.conn;
     }
 
-    public boolean needsUpdate(){
-        if(this.hash == null){
+    public boolean needsUpdate() {
+        if (this.hash == null) {
             return true;
         }
 
-        if(Files.exists(this.output)){
-            if(this.md5){
+        if (Files.exists(this.output)) {
+            if (this.md5) {
                 return !Digester.getMD5(this.output).equalsIgnoreCase(this.hash);
-            } else{
+            } else {
                 return !Digester.getSHA1(this.output).equalsIgnoreCase(this.hash);
             }
         }
@@ -100,39 +109,38 @@ implements Runnable{
     }
 
     @Override
-    public void run(){
-        if(Files.exists(this.output)){
-            if(this.needsUpdate()){
+    public void run() {
+        if (Files.exists(this.output)) {
+            if (this.needsUpdate()) {
                 ATLauncher.LOGGER.info("Updating File " + this.output);
-                try{
+                try {
                     Files.delete(this.output);
-                } catch(IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
-            } else{
+            } else {
                 return;
             }
         }
 
-        try(InputStream stream = this.getStream();
-            FileChannel channel = this.getChannel();
-            ReadableByteChannel rbc = Channels.newChannel(stream)){
+        try (InputStream stream = this.getStream();
+             FileChannel channel = this.getChannel();
+             ReadableByteChannel rbc = Channels.newChannel(stream)) {
 
             channel.transferFrom(rbc, 0, Long.MAX_VALUE);
-        } catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace(System.out);
         }
     }
 
-    private FileChannel getChannel()
-    throws IOException{
+    private FileChannel getChannel() throws IOException {
         return FileChannel.open(this.output, EnumSet.of(StandardOpenOption.CREATE, StandardOpenOption.WRITE));
     }
 
-    private InputStream getStream(){
-        try{
+    private InputStream getStream() {
+        try {
             return (this.isGzip() ? new GZIPInputStream(this.getConnection().getInputStream()) : this.getConnection().getInputStream());
-        } catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace(System.err);
             return this.getConnection().getErrorStream();
         }
